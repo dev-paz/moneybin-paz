505efb146d98ebd4c8d3bda02e1cf66454a38114
Paz <robertparrish@monzo.com>
2019-11-25 21:23:22 +0000

    backend working

--- handlers/router.go
+++ handlers/router.go
@@ -8,5 +8,6 @@ func HandleRequests() {
 	http.HandleFunc("/donations", handleGETDonations)
 	http.HandleFunc("/donate", handlePOSTMakeDonation)
 	http.HandleFunc("/payment_intent", handleGetPaymentIntent)
+	http.HandleFunc("/log_donation", HandleLogDonation)
 
 }
