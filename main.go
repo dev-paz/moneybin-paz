879486de92df109f046ddc69b263970a2690c575
Paz <robertparrish@monzo.com>
2019-11-30 14:05:50 +0000

    change db creds

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ const (
 	port     = 5432
 	user     = "qpivtmamwliml"
 	password = "a3184750c3ee8bb244ecb21efba4a5ecbc0dc442df9f251f657012ead1041aa2"
-	dbname   = "moneybin"
+	dbname   = "dcf8v4unmrcm0k"
 )
 
 func main() {
