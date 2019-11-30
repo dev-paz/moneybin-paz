c71ba68bd137eff68baebd7c0e62020543a64033
Paz <robertparrish@monzo.com>
2019-11-30 14:11:58 +0000

    change db creds

--- models/db.go
+++ models/db.go
@@ -3,6 +3,7 @@ package models
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -11,7 +12,7 @@ var db *sql.DB
 
 func InitDB(psqlInfo string) {
 	var err error
-	db, err = sql.Open("postgres", psqlInfo)
+	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Panic(err)
 	}
