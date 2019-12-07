70b00a8844fd85be1c4009536134e9aca7e36a33
Paz <robertparrish@monzo.com>
2019-12-07 14:02:51 +0000

    redeploy

--- handlers/router.go
+++ handlers/router.go
@@ -5,7 +5,7 @@ import (
 )
 
 func HandleRequests() {
-	http.Handle("/donations", Authorised(handleGETDonations))
+	http.HandleFunc("/donations", handleGETDonations)
 	http.Handle("/donate", Authorised(handlePOSTMakeDonation))
 	http.Handle("/payment_intent", Authorised(handleGetPaymentIntent))
 	http.Handle("/log_donation", Authorised(HandleLogDonation))
